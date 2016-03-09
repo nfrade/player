b3405313a4ee3aaf30687ae78111bd3dfe8f14f0
Youri Daamen <me@youz.io>
2016-03-09 22:03:17 +0100

    solved mpd issue, added mp4 support, source switch between different source types now supported

--- test/index.js
+++ test/index.js
@@ -5,23 +5,49 @@
 var DOM = require('vigour-util/is/node') ? {} : document.body
 var e = require('vigour-element/e')
 var player = require('../lib')
-// var toHTML = require('vdom-to-html')
-// var test = require('tape')
+  // var toHTML = require('vdom-to-html')
+  // var test = require('tape')
 
 var app = e({
   DOM: DOM,
   player: e([{
     components: { icon: { text () { return this.parent.key } } },
-    config: { apiKey: '9ed67940-5265-468d-98f5-f4638258d3c5' }
+    config: { apiKey: '9ed67940-5265-468d-98f5-f4638258d3c5' },
   }, player, require('../lib/bitdash')])
 })
 
 var Observable = require('vigour-observable')
+
 var Data = new Observable({
   inject: require('vigour-observable/lib/data'),
   Child: 'Constructor'
 }).Constructor
 
-app.val = new Data({
-  video: 'http://eu-storage-bitcodin.storage.googleapis.com/bitStorage/2686_1acb6ae99aa947d716463ce5bf3947ce/44855_41fa53de02cf600d6f56ac459dd5f015/44855.{type}'
+// http://akamaivod.tv.ae/42c89/LYX00000007_3848_(3848_ISMUSP).ism/LYX00000007_3848_(3848_ISMUSP)
+// http://akamaivod.tv.ae/6236c/LYX00000009_3846_(3846_ISMUSP).ism/LYX00000009_3846_(3846_ISMUSP)
+// http://akamaivod.tv.ae/5a5ea/LYX00000004_3860_(3860_ISMUSP).ism/LYX00000004_3860_(3860_ISMUSP)
+
+var data = new Data({
+  one: {
+    video: 'http://akamaivod.tv.ae/5a5ea/LYX00000004_3860_(3860_ISMUSP).ism/LYX00000004_3860_(3860_ISMUSP).{type}',
+    // video: 'http://cdn.flowplayer.org/202777/84049-bauhaus.{type}',
+    progress: 0,
+    duration: 1000
+    // video: 'https://cstatic.weborama.fr/advertiser/701/13/73/149/640x360_EtapesVie_SBS_NLBE.mp4'
+  },
+  two: {
+    // video: 'http://akamaivod.tv.ae/6236c/LYX00000009_3846_(3846_ISMUSP).ism/LYX00000009_3846_(3846_ISMUSP).{type}',
+    // video: 'http://akamaivod.tv.ae/5a5ea/LYX00000004_3860_(3860_ISMUSP).ism/LYX00000004_3860_(3860_ISMUSP).{type}',
+    // video: 'http://cdn.flowplayer.org/202777/84049-bauhaus.{type}',
+    progress: 0,
+    duration: 1000,
+    video: 'https://cstatic.weborama.fr/advertiser/701/13/73/149/640x360_EtapesVie_SBS_NLBE.mp4'
+  }
 })
+
+app.val = data.one
+
+setTimeout(function () {
+  app.player.__input = data.two
+  app.player.emit('data')
+}, 4000)
