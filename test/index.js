eaafdfc77f37887b44ce930a32506ea058631fa5
Youri Daamen <me@youz.io>
2016-03-11 15:23:56 +0100

    ads

--- test/index.js
+++ test/index.js
@@ -5,15 +5,36 @@
 var DOM = require('vigour-util/is/node') ? {} : document.body
 var e = require('vigour-element/e')
 var player = require('../lib')
-  // var toHTML = require('vdom-to-html')
-  // var test = require('tape')
+
+// var toHTML = require('vdom-to-html')
+// var test = require('tape')
 
 var app = e({
   DOM: DOM,
   player: e([{
-    components: { icon: { text () { return this.parent.key } } },
-    config: { apiKey: '9ed67940-5265-468d-98f5-f4638258d3c5' },
-  }, player, require('../lib/bitdash')])
+    components: {
+      loader: { text: 'LOADING!' },
+      icon: { text () { return this.parent.key } }
+    },
+    config: {
+      ads: {
+        0: [
+          'http://techslides.com/demos/sample-videos/small.mp4'
+        ],
+        10: [
+          'http://techslides.com/demos/sample-videos/small.mp4',
+          'https://cstatic.weborama.fr/advertiser/701/13/73/149/640x360_EtapesVie_SBS_NLBE.mp4'
+        ]
+      },
+      apiKey: '9ed67940-5265-468d-98f5-f4638258d3c5'
+    }
+  }, player, require('../lib/bitdash'), require('../lib/vast')]),
+  title: {
+    text: {
+      val: 'VIDEO PLAYING',
+      $playerAd: 'AD PLAYING'
+    }
+  }
 })
 
 var Observable = require('vigour-observable')
@@ -23,22 +44,13 @@ var Data = new Observable({
   Child: 'Constructor'
 }).Constructor
 
-// http://akamaivod.tv.ae/42c89/LYX00000007_3848_(3848_ISMUSP).ism/LYX00000007_3848_(3848_ISMUSP)
-// http://akamaivod.tv.ae/6236c/LYX00000009_3846_(3846_ISMUSP).ism/LYX00000009_3846_(3846_ISMUSP)
-// http://akamaivod.tv.ae/5a5ea/LYX00000004_3860_(3860_ISMUSP).ism/LYX00000004_3860_(3860_ISMUSP)
-
 var data = new Data({
   one: {
     video: 'http://akamaivod.tv.ae/5a5ea/LYX00000004_3860_(3860_ISMUSP).ism/LYX00000004_3860_(3860_ISMUSP).{type}',
-    // video: 'http://cdn.flowplayer.org/202777/84049-bauhaus.{type}',
     progress: 0,
     duration: 1000
-    // video: 'https://cstatic.weborama.fr/advertiser/701/13/73/149/640x360_EtapesVie_SBS_NLBE.mp4'
   },
   two: {
-    // video: 'http://akamaivod.tv.ae/6236c/LYX00000009_3846_(3846_ISMUSP).ism/LYX00000009_3846_(3846_ISMUSP).{type}',
-    // video: 'http://akamaivod.tv.ae/5a5ea/LYX00000004_3860_(3860_ISMUSP).ism/LYX00000004_3860_(3860_ISMUSP).{type}',
-    // video: 'http://cdn.flowplayer.org/202777/84049-bauhaus.{type}',
     progress: 0,
     duration: 1000,
     video: 'https://cstatic.weborama.fr/advertiser/701/13/73/149/640x360_EtapesVie_SBS_NLBE.mp4'
@@ -47,7 +59,13 @@ var data = new Data({
 
 app.val = data.one
 
-setTimeout(function () {
-  app.player.__input = data.two
-  app.player.emit('data')
-}, 4000)
+// setTimeout(function () {
+//   app.player.__input = data.two
+//   app.player.emit('data')
+//   app.player.ad.origin.set(true)
+//   app.player.ended.origin.on(function () {
+//     app.player.__input = data.one
+//     app.player.emit('data')
+//     app.player.ad.origin.set(false)
+//   })
+// }, 4000)
